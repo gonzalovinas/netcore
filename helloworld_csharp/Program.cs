69cdc947d661ac00144ada910e3ad5bcd5bbc4e4
GonZa <gonzalo.vinas@gmail.com>
2019-05-20 16:47:44 -0300

    ..

---
+++ helloworld_csharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace helloworld_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo!");
+
+            var l = File.ReadAllLines("ejemplo.txt");
+
+            foreach(var s in l) {
+                Console.WriteLine(s);
+            }
+
+
+            
+            
+        }
+    }
+}
